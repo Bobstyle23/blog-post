b6692ebff08b3cb347bcc9c251af71593fe80f90
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-05-08 19:06:28 +0500

    Setup project and install react & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,md,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: ["'M PLUS Rounded 1c'", "Verdana", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
