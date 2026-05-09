0cd5ad335ec5db147d0c58d262df7e232c12b36f
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-05-09 18:56:08 +0500

    add custom styles & remove posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,36 @@
 // @ts-check
 
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import image from '@astrojs/image';
+import addClasses from 'rehype-add-classes';
 
 export default defineConfig({
   site: 'https://example.com',
   integrations: [sitemap(), react(), image()],
-
+  markdown: {
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-mplus',
+          h2: 'text-2xl font-bold font-mplus',
+          h3: 'text-xl font-bold font-mplus',
+          h4: 'text-lg font-bold font-mplus',
+          h5: 'font-bold font-mplus',
+          h6: 'font-bold font-mplus',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
+        },
+      ],
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
